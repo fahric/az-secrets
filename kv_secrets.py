3d30adf3f141111930ddc7418ddc41df23be5e94
Fahri Cavusoglu <fahric@hotmail.co.uk>
2021-12-14 11:16:35 +0200

    add more env,show error when can not load a secret

--- kv_secrets.py
+++ kv_secrets.py
@@ -12,7 +12,10 @@ OPTIONS = [
 "prov-frontend-expo-qa-config-file",
 "prov-backend-local-env-file",
 "prov-backend-dev-env-file",
-"prov-backend-qa-env-file"
+"prov-backend-qa-env-file",
+"prov-db-mgr-local-env-file",
+"prov-db-mgr-dev-env-file",
+"prov-db-mgr-qa-env-file"
 ] #etc
 
 
@@ -23,42 +26,14 @@ credential = DefaultAzureCredential()
 client = SecretClient(vault_url=KVUri, credential=credential)
 
 
-root = tk.Tk()
-root.title('Compate and update secrets in AZURE')
-root.geometry("1200x600")
-
-selectedSecret = tk.StringVar(root)
-selectedSecret.set(OPTIONS[0]) # default value
-
-
-f = tk.Frame(root, width=80, height=40)
-f.place(x=10, y=20)
-f.pack()
-leftframe = tk.Frame(root)  
-leftframe.pack(side = tk.LEFT)  
-
-rightframe = tk.Frame(root)  
-rightframe.pack(side = tk.RIGHT)  
-  
-  
-# Create label
-label = tk.Label(leftframe, text = "Secret in Azure")
-label.config(font =("Courier", 14))
-label.pack(side=tk.TOP)
-
-
-w = tk.OptionMenu(leftframe, selectedSecret, *OPTIONS)
-w.config(width=40)
-w.pack(side=tk.TOP)
-
-secretValueTextBox = tk.Text(leftframe, height=70, width=90, background="white",insertbackground="black",  foreground="black")
-secretValueTextBox.pack(side=tk.LEFT)
-
-
 def getSecretCallBack():
-    retrieved_secret = client.get_secret(selectedSecret.get())
-    secretValueTextBox.delete('1.0', tk.END)
-    secretValueTextBox.insert(tk.END, retrieved_secret.value)
+    try:
+        retrieved_secret = client.get_secret(selectedSecret.get())
+        secretValueTextBox.delete('1.0', tk.END)
+        secretValueTextBox.insert(tk.END, retrieved_secret.value)
+    except:
+        messagebox.showerror("Error","There was an error getting the secret")
+
 
 def saveSecretCallBack():
     secretValue = secretValueTextBox.get(1.0, "end-1c")
@@ -79,7 +54,38 @@ def getLocalEvnFile():
         data = f.read()
         localSecretValueTextBox.insert(tk.END, data)
 
+
+root = tk.Tk()
+root.title('Compare and update secrets in AZURE')
+root.geometry("1200x600")
+
+selectedSecret = tk.StringVar(root)
+selectedSecret.set(OPTIONS[0]) # default value
+
+
+f = tk.Frame(root, width=80, height=40)
+f.place(x=10, y=20)
+f.pack()
+
+leftframe = tk.Frame(root)  
+leftframe.pack(side = tk.LEFT)  
+
+rightframe = tk.Frame(root)  
+rightframe.pack(side = tk.RIGHT)  
   
+# Create label
+label = tk.Label(leftframe, text = "Secret in Azure")
+label.config(font =("Courier", 14))
+label.pack(side=tk.TOP)
+
+
+w = tk.OptionMenu(leftframe, selectedSecret, *OPTIONS)
+w.config(width=40)
+w.pack(side=tk.TOP)
+
+secretValueTextBox = tk.Text(leftframe, height=70, width=90, background="white",insertbackground="black",  foreground="black")
+secretValueTextBox.pack(side=tk.LEFT)
+
 # Create button for next text.
 getSecretValueButton = tk.Button(leftframe, text = "Get Value",command=getSecretCallBack)
 getSecretValueButton.pack(side=tk.TOP)
@@ -96,10 +102,10 @@ labelRight = tk.Label(rightframe, text = "Local env file")
 labelRight.config(font =("Courier", 14))
 labelRight.pack(side=tk.TOP)
 
-browseButton_Excel = tk.Button(rightframe, text='Import .env File', command=getLocalEvnFile, bg='black', fg='black',
+browseButton_Files = tk.Button(rightframe, text='Import .env File', command=getLocalEvnFile, bg='black', fg='black',
                                font=('helvetica', 12, 'bold'))
   
-browseButton_Excel.pack(side=tk.TOP)
+browseButton_Files.pack(side=tk.TOP)
 
 localSecretValueTextBox = tk.Text(rightframe, height=70, width=90, background="white",insertbackground="black", foreground="black")
 localSecretValueTextBox.pack(side=tk.LEFT)
